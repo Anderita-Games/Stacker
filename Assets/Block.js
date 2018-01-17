58aab4f37a47c9659280d7e99314585c7bb05a51
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-16 20:24:08 -0500

    Added App
    This is the first version of Stacker that GitHub has ever seen!

---
+++ Assets/Block.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+var maxFallDistance = -1;
+
+function Start () {
+
+}
+
+function Update () {
+	if (transform.position.y <= maxFallDistance) {
+		Application.LoadLevel("MainMenu");
+	}
+	if (transform.position.y >= PlayerPrefs.GetInt("Score")) {
+		PlayerPrefs.SetInt("Score", transform.position.y);
+	}
+}
